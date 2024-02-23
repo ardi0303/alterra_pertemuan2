e69defdf7444aed69b9129d574b54609a665f4b4
Ardi Saputra <ardi210303@gmail.com>
2024-02-24 01:31:01 +0700

    commit pertemuan3

---
+++ index.js
@@ -0,0 +1,6 @@
+var name = 'Ardi Saputra';
+var age = 20;
+var univ = 'Universitas Dian Nuswantoro';
+var major = 'Teknik Informatika';
+
+console.log('%cNama saya ' + name + ', umur saya ' + age + ' tahun, saya kuliah di ' + univ + ' jurusan ' + major + '.', 'color: red; font-size: larger');
