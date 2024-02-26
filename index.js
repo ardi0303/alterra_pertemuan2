6ec28c24347393d4eabe76e10a73eb9c3ccce3f6
Ardi Saputra <ardi210303@gmail.com>
2024-02-26 22:14:14 +0700

    commit pertemuan 4 26/02/2024

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
-var name = 'Ardi Saputra';
-var age = 20;
-var univ = 'Universitas Dian Nuswantoro';
-var major = 'Teknik Informatika';
+// var name = 'Ardi Saputra';
+// var age = 20;
+// var univ = 'Universitas Dian Nuswantoro';
+// var major = 'Teknik Informatika';
 
-console.log('%cNama saya ' + name + ', umur saya ' + age + ' tahun, saya kuliah di ' + univ + ' jurusan ' + major + '.', 'color: red; font-size: larger');
+// console.log('%cNama saya ' + name + ', umur saya ' + age + ' tahun, saya kuliah di ' + univ + ' jurusan ' + major + '.', 'color: red; font-size: larger');
+
+var age = 50;
+
+switch (age) {
+    case 50:
+        console.log('Umur saya 50 tahun');
+        break;
+
+    default:
+        console.log('Umur saya tidak 50 tahun');
+        break;
+}
+
+const hitungLuasLingkaran = (r) => {
+    const phi = 3.14;
+    return phi * r * r;
+}
+
+function hitungNilai(nilai, cb){
+    if(nilai <= 65){
+        cb('tidak lolos');
+    } else {
+        cb('lolos');
+    }
+}
+
+hitungNilai(60, (res) => {
+    console.log(res);
+});
